6087282df41c4d368e1665bd564e7c3a08487f22
kanaru0928 <68174393+kanaru0928@users.noreply.github.com>
2021-08-21 22:11:23 +0900

    Update

--- topbit_sort.cpp
+++ topbit_sort.cpp
@@ -1,71 +1,60 @@
+/**
+ * @file topbit_sort.cpp
+ * @brief オリジナルのソート
+ * @author kanaru
+ * @date 2021/8/21
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
-typedef pair<ll, ll> LP;
 typedef unsigned int uint;
 typedef unsigned long long ull;
-#define repi(i, a, b) for(int i = int(a); i < int(b); i++)
-#define rep(i, n) repi(i, 0, n)
-#define repir(i, a, b) for(int i = int(a) - 1; i >= int(b); i--)
-#define repr(i, n) repir(i, n, 0)
-#define INF 1000000000
-#define INF_LL 4000000000000000000
-#define all(x) (x).begin(), (x).end()
-#define UNI(x) (x).erase(unique(all(x)),(x).end())
-#define F first
-#define S second
-#define MP make_pair
-#define MT make_tuple
-#define uset unordered_set
-#define MOD 1000000007
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
 
 #define MAXBIT 32
 
-template<class T> bool chmax(T& a, T b){
-    if(a < b){
-        a = b;
-        return true;
-    }
-    return false;
-}
-
 vector<int> topbit_sort(vector<int> v, vector<int> r){
+    // サイズが1の時はそのまま返す
     if(v.size() <= 1) return r;
+    // サイズが2の時は大小を判定して返す
     if(v.size() == 2){
         if(r[0] > r[1]) swap(r[0], r[1]);
         return r;
     }
-    vector<vector<int>> maxbit(32);
-    vector<vector<int>> maxbit_r(32);
+    //  二進法の桁数ごとに分けるための配列
+    vector<vector<int>> maxbit(MAXBIT);     //  処理用の値
+    vector<vector<int>> maxbit_r(MAXBIT);   //  戻り値用の値
     vector<int> zero;
     int n = v.size();
-    rep(i, n){
+    for(int i = 0; i < n; i++){
         int index;
-        if(!v[i]){
+        if(!v[i]){   // log_2(0)は-∞に発散するので別で保管する
             zero.push_back(r[i]);
             continue;
         }
-        else index = (int)log2((uint)v[i]);
+        else index = (int)log2((uint)v[i]); //  uintにすることで負の値に対応
         maxbit[index].push_back(v[i]);
         maxbit_r[index].push_back(r[i]);
     }
     vector<int> res;
-    if(!maxbit[31].empty()){
-        res = topbit_sort(maxbit[31], maxbit_r[31]);
+    //  並べるときは負、0、正の順
+    if(!maxbit[MAXBIT - 1].empty()){
+        for(int j = 0; j < maxbit[MAXBIT - 1].size(); j++){
+            maxbit[MAXBIT - 1][j] &= ~(1 << MAXBIT - 1);
+        }
+        res = topbit_sort(maxbit[MAXBIT - 1], maxbit_r[MAXBIT - 1]);
     }
     if(!zero.empty()){
-        res.insert(res.end(), all(zero));
+        res.insert(res.end(), zero.begin(), zero.end());
     }
-    rep(i, 31){
+    for(int i = 0; i < MAXBIT - 1; i++){
         if(!maxbit[i].empty()){
-            rep(j, maxbit[i].size()){
+            //  最上位を0にする
+            for(int j = 0; j < maxbit[i].size(); j++){
                 maxbit[i][j] &= ~(1 << i);
             }
             vector<int> tmp = topbit_sort(maxbit[i], maxbit_r[i]);
-            res.insert(res.end(), all(tmp));
+            res.insert(res.end(), tmp.begin(), tmp.end());
         }
     }
     return res;
@@ -82,9 +71,11 @@ int main(){
     // rep(i, n){
     //     cin >> a.at(i);
     // }
+    
+    // int型のみ対応(負でもOK) ちょっと書き換えればuintとかLLとかにも対応できると思います。
     vector<int> a = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1};
     vector<int> ans = topbit_sort(a);
-    rep(i, ans.size()){
+    for(int i = 0; i < ans.size(); i++){
         cout << ans.at(i) << " ";
     }
     cout << endl;
